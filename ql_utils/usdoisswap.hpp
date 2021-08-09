4c0b124722427ad989b40eec0a6959225ca51579
wchang28 <wchang28@hotmail.com>
2021-08-09 17:31:50 -0400

    cleanup instrument.hpp

--- ql_utils/usdoisswap.hpp
+++ ql_utils/usdoisswap.hpp
@@ -3,33 +3,33 @@
 #include <string>
 
 namespace QuantLib {
-	// USD OIS swap index
-	template <typename OvernightIndex>	// OvernightIndex can be FedFunds or Sofr
-	class UsdOvernightIndexedSwapIsdaFix : public
-	#ifdef QL_OVERNIGHT_INDEXED_SWAP_INDEX_MISSING_IMPL
-		QuantLib::OvernightIndexedSwapIndexEx
-	#else
-		QuantLib::OvernightIndexedSwapIndex
-	#endif
-			{
-	public:
-		UsdOvernightIndexedSwapIsdaFix(
-			const QuantLib::Period& tenor,
-			const QuantLib::Handle<QuantLib::YieldTermStructure>& indexEstimateTermStructure = QuantLib::Handle<QuantLib::YieldTermStructure>()
-		) :
-	#ifdef QL_OVERNIGHT_INDEXED_SWAP_INDEX_MISSING_IMPL
-			OvernightIndexedSwapIndexEx
-	#else
-			OvernightIndexedSwapIndex
-	#endif
-			(
-				std::string("UsdOvernightIndexedSwapIsdaFix<<")+ OvernightIndex().name() + ">>",
-				tenor,
-				2,
-				QuantLib::USDCurrency(),
-				QuantLib::ext::shared_ptr<OvernightIndex>(new OvernightIndex(indexEstimateTermStructure)),
-				true,	// could be false ???
-				QuantLib::RateAveraging::Compound
-			) {}
-	};
+    // USD OIS swap index
+    template <typename OvernightIndex> // OvernightIndex can be FedFunds or Sofr
+    class UsdOvernightIndexedSwapIsdaFix : public
+#ifdef QL_OVERNIGHT_INDEXED_SWAP_INDEX_MISSING_IMPL
+        QuantLib::OvernightIndexedSwapIndexEx
+#else
+        QuantLib::OvernightIndexedSwapIndex
+#endif
+    {
+public:
+    UsdOvernightIndexedSwapIsdaFix(
+        const QuantLib::Period& tenor,
+        const QuantLib::Handle<QuantLib::YieldTermStructure>& indexEstimatingTermStructure = QuantLib::Handle<QuantLib::YieldTermStructure>()
+    ) :
+#ifdef QL_OVERNIGHT_INDEXED_SWAP_INDEX_MISSING_IMPL
+        OvernightIndexedSwapIndexEx
+#else
+        OvernightIndexedSwapIndex
+#endif
+        (
+            std::string("UsdOvernightIndexedSwapIsdaFix<<") + OvernightIndex().name() + ">>",
+            tenor,
+            2,
+            QuantLib::USDCurrency(),
+            QuantLib::ext::shared_ptr<OvernightIndex>(new OvernightIndex(indexEstimatingTermStructure)),
+            true, // could be false ???
+            QuantLib::RateAveraging::Compound
+        ) {}
+    };
 }
