c5e71f72f5043d7aec62ddc7c12c6e6f0a25534c
wchang28 <wchang28@hotmail.com>
2022-10-03 00:04:47 -0400

    added simple par rate calculator

--- ql_utils/simple-par-yield-calculator.hpp
+++ ql_utils/simple-par-yield-calculator.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <ql/quantlib.hpp>
+#include <ql_utils/types.hpp>
 #include <algorithm>
+#include <cmath>
+#include <vector>
 
 namespace QLUtils {
 	class SimpleParYieldCalculator {
@@ -44,6 +47,96 @@ namespace QLUtils {
 				}
 				auto parYield = (QuantLib::Rate)((1. - dfLast) / sum);
 				return parYield;
+			}
+		}
+	};
+
+	// calculate spot/forward par rate/yield give a monthly zero rates vector, compounded in some frequency
+	template <
+		RateUnit RATE_UNIT = RateUnit::Percent,
+		QuantLib::Frequency COUPON_FREQ = QuantLib::Frequency::Semiannual
+	>
+	class SimpleParRateCalculator {
+	public:
+		typedef std::vector<double> MonthlyZeroRates;
+	private:
+		const MonthlyZeroRates& monthlyZeroRates_;	// zero rate vectors, compounded in COUPON_FREQ, first element must be the spot zero rate (ie: month=0)
+	public:
+		SimpleParRateCalculator(
+			const MonthlyZeroRates& monthlyZeroRates	// zero rate vectors, compounded in COUPON_FREQ, first element must be the spot zero rate (ie: month=0)
+		) :
+			monthlyZeroRates_(monthlyZeroRates)
+		{
+			auto n = monthlyZeroRates_.size();
+			QL_REQUIRE(n >= 2, "too few zero rate nodes (" << n << "). The minimum is 2");
+		}
+	private:
+		static double multiplier() {
+			auto unit = RATE_UNIT;
+			switch (unit) {
+			case RateUnit::Decimal:
+			default:
+				return 1.;
+			case RateUnit::Percent:
+				return 0.01;
+			case RateUnit::BasisPoint:
+				return 0.0001;
+			}
+		}
+	public:
+		static double couponFrequency() {
+			return (double)COUPON_FREQ;
+		}
+		static size_t couponIntervalMonths() {
+			return (size_t)12 / (size_t)COUPON_FREQ;
+		}
+		double operator() (
+			size_t tenorMonth,
+			size_t fwdMonth = 0
+		) const {
+			auto freq = couponFrequency();
+			auto n = monthlyZeroRates_.size();
+			QL_REQUIRE(tenorMonth > 0, "tenor in month (" << tenorMonth << ") must be greater than zero");
+			QL_REQUIRE(fwdMonth >= 0, "forward in month (" << fwdMonth << ") cannot be negative");
+			auto lastRelevantMonth = fwdMonth + tenorMonth;
+			QL_REQUIRE(lastRelevantMonth < n, "forward+tenor (" << (lastRelevantMonth) << ") is over the limit (" << (n - 1) << ")");
+			auto t_0 = (QuantLib::Time)fwdMonth / 12.;
+			auto zr_0 = monthlyZeroRates_[fwdMonth] * multiplier();
+			auto df_0 = std::pow(1.0 + zr_0 / freq, -t_0 * freq);
+			if (tenorMonth <= 12) {
+				if (fwdMonth == 0) {
+					return monthlyZeroRates_[tenorMonth];
+				}
+				else {
+					auto month = lastRelevantMonth;
+					auto zr = monthlyZeroRates_[month] * multiplier();
+					auto t = (QuantLib::Time)month / 12.;
+					auto df = std::pow(1.0 + zr / freq, -t * freq);
+					df /= df_0;
+					t = (QuantLib::Time)tenorMonth / 12.;
+					zr = (std::pow(df, -1. / (t * freq)) - 1.0) * freq;
+					return zr / multiplier();
+				}
+			}
+			else { // tenorMonth > 12
+				auto cpnIntrvlMonths = couponIntervalMonths();
+				auto start = fwdMonth + (tenorMonth % cpnIntrvlMonths == 0 ? cpnIntrvlMonths : tenorMonth % cpnIntrvlMonths);
+				auto prevMonth = fwdMonth;
+				auto last_df = 1.;
+				auto sum = 0.;
+				for (auto month = start; month <= lastRelevantMonth; month += cpnIntrvlMonths) {
+					auto d_months = month - prevMonth;
+					auto dt = (QuantLib::Time)d_months / 12.;
+					auto zr = monthlyZeroRates_[month] * multiplier();
+					auto t = (QuantLib::Time)month / 12.;
+					auto df = std::pow(1.0 + zr / freq, -t * freq);
+					df /= df_0;
+					sum += dt * df;
+					prevMonth = month;
+					last_df = df;
+				}
+				auto r = (1. - last_df) / sum;
+				return r / multiplier();
 			}
 		}
 	};
--- ql_utils/types.hpp
+++ ql_utils/types.hpp
@@ -10,6 +10,12 @@ namespace QLUtils {
     template<typename _Elem> using string_t = std::basic_string<_Elem>;
     template<typename _Elem> using ostringstream_t = std::basic_ostringstream<_Elem>;
 
+	enum RateUnit {
+		Decimal = 0,
+		Percent = 1,
+		BasisPoint = 2,
+	};
+
 	struct ParYieldTermStructInstrument {
 		virtual QuantLib::Time parTerm() const = 0;
 		virtual QuantLib::Rate parYield() const = 0;
