1791b840f7e59e4628811214f665bca5e6746fc7
Wen Chang <wchang@cerberus.com>
2023-10-04 18:14:50 -0400

    updated

--- ql_utils/indexes/ibor/usdlibor-3m.hpp
+++ ql_utils/indexes/ibor/usdlibor-3m.hpp
@@ -3,6 +3,7 @@
 
 namespace QuantLib {
     class USDLibor3M : public USDLibor {
+    public:
         USDLibor3M(
             const Handle<YieldTermStructure>& h = {}
         ) : USDLibor(3 * Months, h)
