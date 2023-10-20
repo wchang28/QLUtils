56c6296cfd27a0e28bc69a4fb9063e6e2708eaf3
Wen Chang <wchang@cerberus.com>
2023-10-20 14:34:45 -0400

    updated

--- ql_utils/simple-par-yield-calculator.hpp
+++ ql_utils/simple-par-yield-calculator.hpp
@@ -7,50 +7,6 @@
 #include <cmath>
 
 namespace QLUtils {
-	class SimpleParYieldCalculator {
-	public:
-		static QuantLib::Rate parYield(
-			const QuantLib::YieldTermStructure& yieldCurve,
-			const std::size_t& tenorMonth,
-			const QuantLib::Period& forward = QuantLib::Period(0, QuantLib::Days),
-			const QuantLib::Frequency parCouponFreq = QuantLib::Semiannual
-		) {
-			QL_ASSERT(tenorMonth > 0, "par tenor in month must be greater than 0");
-			auto today = yieldCurve.referenceDate();
-			auto settlemenDate = today + forward;
-			QuantLib::Period tenor(tenorMonth, QuantLib::Months);
-			auto maturityDate = settlemenDate + tenor;
-			auto dfSettle = yieldCurve.discount(settlemenDate);
-			if (tenorMonth <= 12) {	// for tenor <= 1yr, zero rate is par yield
-				auto zeroRate = yieldCurve.forwardRate(
-					settlemenDate,
-					maturityDate,
-					QuantLib::Thirty360(QuantLib::Thirty360::ISDA),
-					QuantLib::Compounded,
-					parCouponFreq
-				);
-				auto parYield = (QuantLib::Rate)zeroRate;
-				return parYield;
-			}
-			else {	// for tenor > 1yr => none-zero coupon, price at par
-				int couponIntervalMonths = 12 / (int)parCouponFreq;
-				auto sum = 0.;
-				auto dfLast = 1.;
-				for (int i = (int)tenorMonth; i > 0; i -= couponIntervalMonths) { // for each cashflow at coupon interval
-					if (i == tenorMonth) {	// at maturity date
-						dfLast = yieldCurve.discount(maturityDate) / dfSettle;
-					}
-					auto cf_date = settlemenDate + QuantLib::Period(i, QuantLib::Months);
-					auto df = yieldCurve.discount(cf_date) / dfSettle;
-					auto dt = (double)(std::min(couponIntervalMonths, i)) / 12.0;
-					sum += dt * df;
-				}
-				auto parYield = (QuantLib::Rate)((1. - dfLast) / sum);
-				return parYield;
-			}
-		}
-	};
-
 	// calculate spot/forward par rate/yield give a monthly zero rates vector, compounded in some frequency
 	template <
 		RateUnit RATE_UNIT = RateUnit::Percent,
