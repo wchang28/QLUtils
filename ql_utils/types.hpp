effa914882bb7290fa148cf645504be2e22d1a4b
Wen Chang <wchang@cerberus.com>
2023-10-24 11:49:30 -0400

    updated

--- ql_utils/types.hpp
+++ ql_utils/types.hpp
@@ -6,6 +6,7 @@
 #include <functional>
 #include <ostream>
 #include <vector>
+#include <memory> 
 
 namespace QLUtils {
     template<typename _Elem> using string_t = std::basic_string<_Elem>;
@@ -51,13 +52,133 @@ namespace QLUtils {
 		}
 		void assertValid() const {
 			QL_ASSERT(maturities.size() == rates.size(), "the length of maturities (" << maturities.size() << " is different from the length of rates (" << rates.size() << ")");
-			QL_ASSERT(size() > 0, "term structure is empty");
+			QL_ASSERT(!empty(), "term structure is empty");
 		}
 	};
 
-	typedef std::vector<double> MonthlyZeroRates;
-	typedef std::vector<double> MonthlyForwardCurve;
-	typedef std::vector<double> MonthlyRates;
+	template <
+		typename TERM_TYPE = QuantLib::Time,
+		typename RATE_TYPE = QuantLib::Rate
+	>
+	struct TermStructureNode {
+		typedef TERM_TYPE TermType;
+		typedef RATE_TYPE RateType;
+		TermType term;
+		RateType rate;
+		TermStructureNode(
+			TermType term = 0,
+			RateType rate = 0
+		) :term(term), rate(rate)
+		{}
+	};
+
+    template <
+        typename TERM_TYPE = QuantLib::Time,
+        typename RATE_TYPE = QuantLib::Rate
+    >
+    class TermStructureNodes {
+    public:
+        typedef TERM_TYPE TermType;
+        typedef RATE_TYPE RateType;
+        typedef TermStructureNode<TermType, RateType> Node;
+        typedef std::shared_ptr<Node> pNode;
+        typedef std::vector<pNode> Nodes;
+        typedef std::shared_ptr<Nodes> pNodes;
+        typedef std::vector<TermType> TermVector;
+        typedef std::vector<RateType> RateVector;
+        typedef std::shared_ptr<TermVector> pTermVector;
+        typedef std::shared_ptr<RateVector> pRateVector;
+        typedef std::pair<pTermVector, pRateVector> TermRateVectorsPair;
+    private:
+        pNodes pNodes_;
+    public:
+        TermStructureNodes(
+            const pNodes& pNodes = nullptr
+        ) : pNodes_(pNodes)
+        {}
+        TermStructureNodes(
+            const Nodes& nodes
+        ) : pNodes_(new Nodes(nodes))
+        {}
+        const pNodes& pTSNodes() const {
+            return pNodes_;
+        }
+        pNodes& pTSNodes() {
+            return pNodes_;
+        }
+        const Nodes& nodes() const {
+            QL_ASSERT(pNodes_ != nullptr, "term structure's nodes is null");
+            return *pNodes_;
+        }
+        bool empty() const {
+            return nodes().empty();
+        }
+        bool size() const {
+            return nodes().size();
+        }
+        void resize(size_t n) {
+            if (pNodes_ == nullptr) {
+                pNodes_.reset(new Nodes(n));
+            }
+            else {
+                pNodes_->resize(n);
+            }
+        }
+        void assertValid() const {
+            QL_ASSERT(!empty(), "term structure is empty");
+        }
+    private:
+        void transferToVectorsPair(
+            TermVector& termVector,
+            RateVector& rateVector
+        ) const {
+            const auto& nodes = this->nodes();
+            auto n = nodes.size();
+            QL_ASSERT(termVector.size() == n, "size of the term vector (" << termVector.size() << ") is not what's expected (" << n << ")");
+            QL_ASSERT(rateVector.size() == n, "size of the rate vector (" << rateVector.size() << ") is not what's expected (" << n << ")");
+            for (decltype(n) i = 0; i < n; ++i) {   // for each node
+                try {
+                    const auto& pNode = nodes.at(i);
+                    QL_ASSERT(pNode != nullptr, "term structure node is null");
+                    termVector[i] = pNode->term;
+                    rateVector[i] = pNode->rate;
+                }
+                catch (const std::exception& e) {
+                    QL_FAIL("TS node[" << i << "]: " << e.what());
+                }
+            }
+        }
+    public:
+        operator TermRateVectorsPair() const {
+            assertValid();
+            auto n = nodes().size();
+            TermRateVectorsPair ret;
+            ret.first.reset(new TermVector(n));
+            ret.second.reset(new RateVector(n));
+            auto& termVector = *(ret.first);
+            auto& rateVector = *(ret.second);
+            transferToVectorsPair(termVector, rateVector);
+            return ret;
+        }
+        operator std::shared_ptr<YieldTSNodes<TermType, RateType>>() const {
+            assertValid();
+            auto n = nodes().size();
+            std::shared_ptr<YieldTSNodes<TermType, RateType>> ret(new YieldTSNodes<TermType, RateType>(n));
+            auto& termVector = ret->maturities;
+            auto& rateVector = ret->rates;
+            transferToVectorsPair(termVector, rateVector);
+            return ret;
+        }
+    };
+    template <
+        typename TERM_TYPE = QuantLib::Time,
+        typename RATE_TYPE = QuantLib::Rate
+    >
+    using pTermStructureNodes = std::shared_ptr<TermStructureNodes<TERM_TYPE, RATE_TYPE>>;
+
+	typedef std::vector<QuantLib::Real> MonthlyZeroRates;
+	typedef std::vector<QuantLib::Real> MonthlyForwardCurve;
+	typedef std::vector<QuantLib::Real> MonthlyRates;
 	typedef MonthlyRates HistoricalMonthlyRates;
 
 	struct ParYieldTermStructInstrument {
