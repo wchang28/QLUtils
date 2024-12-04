7b0e1cf8f081024f4f74a365ec81ec34b9aa5c7d
Wen Chang <wchang@cerberus.com>
2024-12-04 08:22:49 -0500

    updated

--- ql_utils/governmentbondtraits.hpp
+++ ql_utils/governmentbondtraits.hpp
@@ -1,33 +1,63 @@
 #pragma once
-
+// traits for goverment secutities
+// must use template specialization to provide the actual implementation for each currency that support government securities
 #include <ql/quantlib.hpp>
+#include <stdexcept>
 
 namespace QLUtils {
     template <typename QL_CURRENCY>
     struct GovernmentSecurityTraits {
-        QuantLib::Calendar settlementCalendar(const QuantLib::Period& tenor) const {}
-        QuantLib::Natural settlementDays(const QuantLib::Period& tenor) const {}
-        QuantLib::Real parNotional(const QuantLib::Period& tenor) const {}
+        QuantLib::Calendar settlementCalendar(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::Natural settlementDays(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::Real parNotional(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
     };
 
     template <typename QL_CURRENCY>
     struct GovernmentBillTraits {
         typedef GovernmentSecurityTraits<QL_CURRENCY> SecurityTraits;
-        QuantLib::DayCounter dayCounter(const QuantLib::Period& tenor) const {}
-        QuantLib::Frequency bondEquivCouponFrequency(const QuantLib::Period& tenor) const {}
-        QuantLib::DayCounter discountRateDayCounter(const QuantLib::Period& tenor) const {}
-        QuantLib::DayCounter parYieldSplineDayCounter(const QuantLib::Period& tenor) const {}
+        QuantLib::DayCounter dayCounter(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::Frequency bondEquivCouponFrequency(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::DayCounter discountRateDayCounter(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::DayCounter parYieldSplineDayCounter(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
     };
 
     template <typename QL_CURRENCY>
     struct GovernmentBondTraits {
         typedef GovernmentSecurityTraits<QL_CURRENCY> SecurityTraits;
-        QuantLib::Frequency couponFrequency(const QuantLib::Period& tenor) const {}
-        QuantLib::DayCounter accruedDayCounter(const QuantLib::Period& tenor) const {}
-        bool endOfMonth(const QuantLib::Period& tenor) const {}
-        QuantLib::Calendar scheduleCalendar(const QuantLib::Period& tenor) const {}
-        QuantLib::BusinessDayConvention convention(const QuantLib::Period& tenor) const {}
-        QuantLib::BusinessDayConvention terminationDateConvention(const QuantLib::Period& tenor) const {}
-        QuantLib::DayCounter parYieldSplineDayCounter(const QuantLib::Period& tenor) const {}
+        QuantLib::Frequency couponFrequency(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::DayCounter accruedDayCounter(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        bool endOfMonth(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::Calendar scheduleCalendar(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::BusinessDayConvention convention(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::BusinessDayConvention terminationDateConvention(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
+        QuantLib::DayCounter parYieldSplineDayCounter(const QuantLib::Period& tenor) const {
+            throw std::logic_error("not implemented");
+        }
     };
 }
